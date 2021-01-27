2f9650e06c10fa0e459cb8e5e70860bfcb77ecb1
hanxiaotong-cuc <77670368+hanxiaotong-cuc@users.noreply.github.com>
2021-01-27 22:36:58 +0800

    Add files via upload

---
+++ 源1.cpp
@@ -0,0 +1,29 @@
+/*函数定义的五个步骤：
+1.返回值类型
+2.函数名
+3.参数表列
+4.函数体语句
+5.return表达式
+*/
+#include <iostream>
+using namespace std;
+int add(int num1, int num2
+	{
+		int sum = num1 + num2;
+return sum;
+	}
+
+
+	int main(){
+		int a = 10;
+		int b = 20;
+		int c = add(a, b);
+		cout << c << endl;
+
+
+
+
+	}
+
+	system("pause");
+	return 0;
